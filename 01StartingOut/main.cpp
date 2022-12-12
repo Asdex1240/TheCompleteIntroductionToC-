dc612a6dcd0a53a6ea8b10d6df4ba2194c1aba6f
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2022-12-12 16:08:39 -0600

    Aprendimos manejo de strings, metodos max_size, size, find, clear

--- 01StartingOut/main.cpp
+++ 01StartingOut/main.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 int main(){
 
-    string food1 = "Pizza";
-    string food2 = "Pasta";
-    string food3 = "Burger";
-    
-    cout<<food1 << food2 << food3<<endl;
+    string try1 = "a simple sentence hello world! 2 3 4" ;
+    double size2 = try1.max_size();
+    double size1 = try1.size();
+    int find1 = try1.find("simple",0);
+    try1.clear();
 
-    cout<<"Today I ate "<< food1<<endl;
-    cout<<"I really love "<<food2<<endl;
-    cout<<food3<<" is great"<<endl;
-    cout<<"My favorite food is "<<food1<<endl;
+    cout << size1 << endl;
+    cout << size2 << endl;
+    cout << find1 << endl;
 
     return 0;
 }
